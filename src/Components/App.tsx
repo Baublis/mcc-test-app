4aaf329ca78fb5ce797e3d31e231c64632b92005
BaublisD <BaublisDuos@gmail.com>
2025-04-02 12:59:51 +0700

    Init

---
+++ src/Components/App.tsx
@@ -0,0 +1,9 @@
+import {FC} from "react";
+
+const App: FC = () => (
+    <div id="app-main-layout">
+        Hello world!
+    </div>
+);
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./Components/App.tsx";
+
+const htmlElement = document.getElementById('root');
+
+htmlElement &&
+createRoot(htmlElement).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
